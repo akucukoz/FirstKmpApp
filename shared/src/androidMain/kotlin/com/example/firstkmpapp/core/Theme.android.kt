7c6f5858cc7fff799a8a08fb75d63a94a67601e3
Arda Kucukoz <kucukozarda@gmail.com>
2025-01-01 21:55:32 -0500

    initial step on iOS UI - win completed

--- shared/src/androidMain/kotlin/com/example/firstkmpapp/core/Theme.android.kt
+++ shared/src/androidMain/kotlin/com/example/firstkmpapp/core/Theme.android.kt
@@ -1,9 +1,15 @@
 package com.example.firstkmpapp.core
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 @Composable
 actual fun ContactTheme(content: @Composable () -> Unit) {
-    MaterialTheme(content = content)
+    val colorScheme =
+        if (isSystemInDarkTheme()) darkColorScheme()
+        else lightColorScheme()
+    MaterialTheme(colorScheme = colorScheme, content = content)
 }
--- shared/src/iosMain/kotlin/com/example/firstkmpapp/core/Theme.ios.kt
+++ shared/src/iosMain/kotlin/com/example/firstkmpapp/core/Theme.ios.kt
@@ -1,7 +1,19 @@
 package com.example.firstkmpapp.core
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import platform.UIKit.UIScreen
+import platform.UIKit.UIUserInterfaceStyle
 
 @Composable
 actual fun ContactTheme(content: @Composable () -> Unit) {
+    val isDarkTheme =
+        UIScreen.mainScreen.traitCollection.userInterfaceStyle ==
+                UIUserInterfaceStyle.UIUserInterfaceStyleDark
+    val colorScheme =
+        if (isDarkTheme) darkColorScheme()
+        else lightColorScheme()
+    MaterialTheme(colorScheme = colorScheme, content = content)
 }
